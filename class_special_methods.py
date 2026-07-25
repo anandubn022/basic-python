3c743a8c92641ab82c1a86ca4d5d5381b9329788
anandubn022 <anandubn022@gmail.com>
2026-07-25 19:03:17 +0530

    Update class_special_methods.py

--- class_special_methods.py
+++ class_special_methods.py
@@ -7,10 +7,12 @@ class Employee:
 
     def fullname(self):
         return self.first + " " + self.last
-    
+
+    # official string representation of object
     def __repr__(self):
         return f"Employee({self.first}, {self.last}, {self.pay})"
-    
+
+    # human readale representaion of object
     def __str__(self):
         return f"{self.fullname()} - {self.pay}"
     
